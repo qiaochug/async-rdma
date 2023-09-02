431dd3a036f5e3bd0c2a1ff5e2b89d9438e9ec38
qiaochug <qiaochug@gmail.com>
2023-09-02 00:01:51 -0400

    WIP save

---
+++ examples/benchmark_tool.rs
@@ -0,0 +1,125 @@
+use async_rdma::{
+    LocalMr, RemoteMr,
+    LocalMrReadAccess, LocalMrWriteAccess, 
+    Rdma, RdmaBuilder};
+use clippy_utilities::Cast;
+use std::{alloc::Layout, env, io::{self, Write}, process::exit};
+use rand::Rng;
+
+async fn prepare_mrs(rdma: &Rdma, local_mrs: &mut Vec<LocalMr>, remote_mrs: &mut Vec<RemoteMr>, req_num: usize, req_size: usize, iamserver: bool) -> io::Result<()>{
+
+    for i in 0..req_num {
+        // Allocating local memory
+        let lmr = rdma.alloc_local_mr(Layout::array::<u8>(req_size).unwrap())?;
+        local_mrs.push(lmr);
+
+        // write data into lmr
+        let fill = if iamserver { 1_u8 } else { 0_u8 };
+        let mut write_access = local_mrs[i].get_mut(0..req_size).unwrap();
+        let mut slice = write_access.as_mut_slice();
+        for byte in slice.iter_mut() {
+            *byte = fill;
+        }
+
+        // Print the first and last bytes
+        println!("First and last bytes are {:?} and {:?}, size {}", slice.get(0), slice.get(slice.len() - 1), slice.len());      
+    }
+
+    Ok(())
+}
+
+
+#[tokio::main]
+async fn main() {
+    println!("Benchmark tool start");
+
+    let args: Vec<String> = env::args().collect();
+    
+    let mut iamserver = false;
+    let mut ip = "172.29.0.118";
+    let mut port = "19875";
+    let mut req_num = 8;
+    let mut req_size = 4096;
+    let mut qp_num = 1;
+    let mut read_pct = 1.0;
+
+    let usage = r#"Usage: cargo run --example client_configurable iamserver=<0or1> host=<server-addr>
+     port=<server-port> req_num=<req_num> req_size=<req_size>
+      qp_num=<qp_num> read_pct=<read_pct>"#;
+
+    for arg in args.iter().skip(1) {
+        if arg.starts_with("iamserver=") {
+            iamserver = arg[10..].parse().unwrap();
+        } else if arg.starts_with("host=") {
+            ip = &arg[5..];
+        } else if arg.starts_with("port=") {
+            port = &arg[5..];
+        } else if arg.starts_with("req_num=") {
+            req_num = arg[8..].parse().unwrap();
+        } else if arg.starts_with("req_size=") {
+            req_size = arg[9..].parse().unwrap();
+        } else if arg.starts_with("qp_num=") {
+            qp_num = arg[8..].parse().unwrap();
+        } else if arg.starts_with("read_pct=") {
+            read_pct = arg[10..].parse().unwrap();
+        } else {
+            println!("{}", usage);
+        }
+    }
+    let addr = format!("{}:{}", ip, port);
+
+    println!("I am server: {}", iamserver);
+    println!("Server IP: {}", ip);
+    println!("Port: {}", port);
+    println!("Addr: {}", addr);
+    println!("Request Number: {}", req_num);
+    println!("Request Size: {}", req_size);
+    println!("QP Number: {}", qp_num);
+    println!("Read Percentage: {}", read_pct);
+
+    let rdma_builder = RdmaBuilder::default().set_dev("mlx5_0").set_imm_flag_in_wc(2).unwrap();
+    let mut rdma;
+    if iamserver {
+        rdma = rdma_builder.listen(addr).await.unwrap();
+        println!("Server up!");
+    } else {
+        rdma = rdma_builder.connect(addr.clone()).await.unwrap();
+        println!("Client up!");
+    }
+
+    let mut local_mrs = Vec::with_capacity(req_num);
+    let mut remote_mrs = Vec::with_capacity(req_num);
+    prepare_mrs(&rdma, &mut local_mrs, &mut remote_mrs, req_num, req_size, iamserver).await.unwrap();
+
+    // let mut rng = rand::thread_rng();
+    // let mut operation_list: Vec<i32> = Vec::new();
+
+    // // Generate random 0s and 1s to represent read (1) or write (0)
+    // let mut read_num = 1;
+    // for _ in 0..req_num {
+    //     let random_value: f64 = rng.gen();
+    //     if random_value < read_pct {
+    //         operation_list.push(1);
+    //         read_num += 1;
+    //     } else {
+    //         operation_list.push(0);
+    //     }
+    // }
+    // let effective_read_pct = read_num as f64/req_num as f64;
+    // println!("Effective Read_pct {:.2}:", effective_read_pct);
+
+    // for i in 0..req_num {
+
+    //     if operation_list[i] == 1 {
+    //         println!("Performing read operation");
+    //     } else {
+    //         println!("Performing write operation");
+    //     }
+
+    // }
+    if iamserver {
+        println!("Server done!");
+    } else {
+        println!("Client done!");
+    }
+}
