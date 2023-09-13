074165ea8daa2128d8ab6fe8aef6a183ab0acd43
qiaochug <qiaochug@gmail.com>
2023-09-13 18:16:14 -0400

    enable thread number

--- examples/benchmark_tool.rs
+++ examples/benchmark_tool.rs
@@ -223,6 +223,7 @@ async fn run_experiment(
     Ok(())
 }
 
+#[derive(Debug)]
 struct summary_stat {
     len: f64,
     sum: f64,
@@ -261,57 +262,19 @@ fn generate_filename(req_num: usize, req_size: usize, read_pct: f64, qp_num: usi
     )
 }
 
-#[tokio::main]
-async fn main() {
-    println!("Benchmark tool start");
+async fn main_async(cfg: config) {
 
-    let args: Vec<String> = env::args().collect();
-    
-    let mut iamserver = false;
-    let mut ip = "172.29.0.118";
-    let mut port = "19875";
-    let mut req_num = 8;
-    let mut req_size = 4096;
-    let mut qp_num = 1;
-    let mut read_pct = 1.0;
-    let mut thread_num = 1;
+    println!("{:?}", cfg);
 
-    let usage = r#"Usage: cargo run --example client_configurable iamserver=<0or1> host=<server-addr>
-     port=<server-port> req_num=<req_num> req_size=<req_size>
-      qp_num=<qp_num> read_pct=<read_pct>"#;
-
-    for arg in args.iter().skip(1) {
-        if arg.starts_with("iamserver=") {
-            iamserver = arg[10..].parse().unwrap();
-        } else if arg.starts_with("host=") {
-            ip = &arg[5..];
-        } else if arg.starts_with("port=") {
-            port = &arg[5..];
-        } else if arg.starts_with("req_num=") {
-            req_num = arg[8..].parse().unwrap();
-        } else if arg.starts_with("req_size=") {
-            req_size = arg[9..].parse().unwrap();
-        } else if arg.starts_with("qp_num=") {
-            qp_num = arg[7..].parse().unwrap();
-        } else if arg.starts_with("read_pct=") {
-            read_pct = arg[9..].parse().unwrap();
-        } else if arg.starts_with("thread_num=") {
-            thread_num = arg[11..].parse().unwrap();
-        } else {
-                println!("{}", usage);
-            }
-    }
-    let addr = format!("{}:{}", ip, port);
-
-    println!("I am server: {}", iamserver);
-    println!("Server IP: {}", ip);
-    println!("Port: {}", port);
-    println!("Addr: {}", addr);
-    println!("Request Number: {}", req_num);
-    println!("Request Size: {}", req_size);
-    println!("QP Number: {}", qp_num);
-    println!("Read Percentage: {}", read_pct);
-    println!("Thread Number: {}", thread_num);
+    let config {
+        iamserver,
+        addr,
+        req_num,
+        req_size,
+        qp_num,
+        read_pct,
+        thread_num
+    } = cfg;
 
     // Prepare QPs
     let rdma_builder = RdmaBuilder::default().set_dev("mlx5_0").set_imm_flag_in_wc(2).unwrap();
@@ -405,4 +368,86 @@ async fn main() {
     } else {
         println!("Client done!");
     }
+
+}
+
+#[derive(Debug)]
+struct config {
+    iamserver: bool,
+    addr: String,
+    req_num: usize,
+    req_size: usize,
+    qp_num: usize,
+    read_pct: f64,
+    thread_num: usize
+}
+
+fn main() {
+
+    println!("Benchmark tool start");
+
+    let args: Vec<String> = env::args().collect();
+    
+    let mut iamserver = false;
+    let mut ip = "172.29.0.118".to_string();
+    let mut port = "19875".to_string();
+    let mut req_num = 8;
+    let mut req_size = 4096;
+    let mut qp_num = 1;
+    let mut read_pct = 1.0;
+    let mut thread_num = 1;
+
+    let usage = r#"Usage: cargo run --example client_configurable iamserver=<0or1> host=<server-addr>
+     port=<server-port> req_num=<req_num> req_size=<req_size>
+      qp_num=<qp_num> read_pct=<read_pct>"#;
+
+    for arg in args.iter().skip(1) {
+        if arg.starts_with("iamserver=") {
+            iamserver = arg[10..].parse().unwrap();
+        } else if arg.starts_with("host=") {
+            ip = arg[5..].to_string();
+        } else if arg.starts_with("port=") {
+            port = arg[5..].to_string();
+        } else if arg.starts_with("req_num=") {
+            req_num = arg[8..].parse().unwrap();
+        } else if arg.starts_with("req_size=") {
+            req_size = arg[9..].parse().unwrap();
+        } else if arg.starts_with("qp_num=") {
+            qp_num = arg[7..].parse().unwrap();
+        } else if arg.starts_with("read_pct=") {
+            read_pct = arg[9..].parse().unwrap();
+        } else if arg.starts_with("thread_num=") {
+            thread_num = arg[11..].parse().unwrap();
+        } else {
+                println!("{}", usage);
+            }
+    }
+    let addr = format!("{}:{}", ip, port).to_string();
+
+    println!("I am server: {}", iamserver);
+    println!("Server IP: {}", ip);
+    println!("Port: {}", port);
+    println!("Addr: {}", addr);
+    println!("Request Number: {}", req_num);
+    println!("Request Size: {}", req_size);
+    println!("QP Number: {}", qp_num);
+    println!("Read Percentage: {}", read_pct);
+    println!("Thread Number: {}", thread_num);
+
+    let cfg = config {
+        iamserver,
+        addr,
+        req_num,
+        req_size,
+        qp_num,
+        read_pct,
+        thread_num
+    };
+
+    tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(thread_num)
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(main_async(cfg));
 }
